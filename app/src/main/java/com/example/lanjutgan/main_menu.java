f6753a4bb89c48e38a50e3f5c0df9fe5bccb2d8f
Dea Avega Editya <dedi0003.student.monash.edu>
2022-02-14 00:42:43 +0700

    add view
    Signed-off-by: Dea Avega Editya <dedi0003.student.monash.edu>

---
+++ app/src/main/java/com/example/lanjutgan/main_menu.java
@@ -0,0 +1,17 @@
+package com.example.lanjutgan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class main_menu extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+
+
+}
